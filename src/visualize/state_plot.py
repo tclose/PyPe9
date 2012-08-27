caa99ef0e17f469cb1238e89f5ed9422e4fa29c3
Tom Close <tclose@oist.jp>
2012-08-27 17:11:30 +0900

    Got state_plot to work for exported states

--- src/visualize/state_plot.py
+++ src/visualize/state_plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Contains a method for plotting cell positions loaded from BRep export files
+Contains a method for plotting saved states from NMODL printouts 
 
 @author Tom Close
 
@@ -12,8 +12,10 @@ Contains a method for plotting cell positions loaded from BRep export files
 #
 #######################################################################################
 
+MAX_NUM_POINTS = 25
+
 import sys
-import numpy
+import math
 import matplotlib.pyplot as plt
 import argparse
 
@@ -21,52 +23,96 @@ parser = argparse.ArgumentParser(description='A script to plot spike activity')
 parser.add_argument('filename', help='The file to plot the spikes from')
 parser.add_argument('--time_start', type=float, default=None, help='The start of the plot')
 parser.add_argument('--time_stop', type=float, default=None, help='The stop of the plot')
-parser.add_argument('--v_incr', type=float, default=0.1, help='The minimum increment required before the next step in the voltage trace is plotted')
+parser.add_argument('--incr', type=float, default=0.01, help='The minimum increment required before the next step in the voltage trace is plotted as a fraction of the starting value')
 parser.add_argument('--extra_label', type=str, default='', help='Additional label information')
 args = parser.parse_args()
 
-# Load voltages selectively, if the difference between previous voltage point exceeds args.v_incr
-f = open(args.filename)
+max_values = {}
+min_values = {}
 time_i = 0
 prev_ID = None
-voltages = []
-times = []
-IDs = []
+states = {}
+times = {}
+prev_x = {}
+prev_t = {}
+order_of_mags= {}
+incrs = {}
+last_x = {}
+last_t = {}
+f = open(args.filename)
+# Loop through and get the maximum and minimum value for each ID
 for line in f:
-    if line[0] != '#': # Check to see if the line is a comment
-        v, ID = line.split()
-        v = float(v)
-        # If the ID signifies the start of a new cell reset the time index
-        if ID != prev_ID:
-            # If not in the initial loop, append last value/time pair to fill out the plot of the previous ID out to the right 
-            if prev_ID != None:
-                voltages[-1].append(v)
-                times[-1].append(time_i * file_header['dt'])
-            time_i = 0
-            prev_v = v - 2.0 * args.v_incr
-            prev_ID = ID
-            voltages.append([])
-            times.append([])
-            IDs.append(int(float(ID)))
-        # If the voltage change is greater than the specified v_incr add it to the vector
-        if abs(v - prev_v) > args.v_incr:
-            voltages[-1].append(v)
-            times[-1].append(time_i * file_header['dt'])
-            prev_v = v
-        time_i += 1
-# Append last value/time pair to fill out plot of the final ID to the right
-voltages[-1].append(v)
-times[-1].append(time_i * file_header['dt'])
-if not voltages:
+    if line.startswith('state: '):
+        try:
+            t, ID, x = line[7:].split()
+        except ValueError:
+            raise Exception("Incorrectly formatted line '%s', should be 'time ID state_value'.")
+        x = float(x)
+        if not max_values.has_key(ID):
+            max_values[ID] = x
+            min_values[ID] = x
+        elif x > max_values[ID]:
+            max_values[ID] = x
+        elif x < min_values[ID]:
+            min_values[ID]
+for ID in max_values.keys():
+    if not max_values[ID] and not min_values[ID]:
+        order_of_mags[ID] = 1.0
+    else:
+        abs_max = max(abs(min_values[ID]), abs(max_values[ID]))
+        order_of_mags[ID] = 10.0 ** math.floor(math.log(abs_max,10.0)) # Calculate the order of magnitude of the maximum absolute value
+    value_range = max_values[ID] - min_values[ID]
+    incrs[ID] = order_of_mags[ID] * args.incr
+f.seek(0) # Rewind file back to beginning
+# Load states selectively, if the difference between previous voltage point exceeds args.x_incr
+for line in f:
+    if line.startswith('state: '): # Check to see if the line is state printout
+        try:
+            t, ID, x = line[7:].split()
+        except ValueError:
+            raise Exception("Incorrectly formatted line '%s', should be 'time ID state_value'.")
+        t = float(t)
+        x = float(x)
+        if not states.has_key(ID):
+            states[ID] = []
+            times[ID] = []
+            prev_x[ID] = x + 2.0 * incrs[ID]
+        if abs(x - prev_x[ID]) > incrs[ID]:
+            states[ID].append(x / order_of_mags[ID])
+            times[ID].append(t)
+            prev_x[ID] = x 
+            prev_t[ID] = t
+        last_x[ID] = x
+        last_t[ID] = t
+f.close()
+if not states:
     print "No trace was loaded from file"
     sys.exit(0)
-# Plot voltages sorted in order of their IDs
-sorted_IDs = []
-for t, v, ID in sorted(zip(times, voltages, IDs), key=lambda line: int(float(line[2]))):
-    plt.plot(t, v)
-    sorted_IDs.append(ID)
-plt.legend(sorted_IDs)
-plt.title(file_header['label'] + ' ' + args.extra_label + ' - States v Time')
+# Append last value/time pair to fill out plot of the final ID to the right
+for ID in last_x.keys():
+    states[ID].append(last_x[ID] / order_of_mags[ID])
+    times[ID].append(last_t[ID])
+    print "'%s' has %d points (range %g, incr %g)" % (ID,len(states[ID]), max_values[ID] - min_values[ID], incrs[ID])
+# Plot states sorted in order of their IDs
+legend = []
+for ID in sorted(states.keys()):
+    state = states[ID]
+    time = times[ID]
+    num_points = len(state)
+    assert(len(time) == num_points)
+    # If the number of points exceeds the maxium, decimate the 
+    if num_points > MAX_NUM_POINTS:
+        incr = num_points / MAX_NUM_POINTS
+        state = state[0:incr:num_points]
+        time = time[0:incr:num_points]
+        print "Warning state '%s' had to have its time course decimated, likely to have large magnitude high frequency oscillations."
+    len(time)
+    len(state)
+    plt.plot(time, state)
+    legend.append(ID + ' x 10^' + str(math.log(order_of_mags[ID],10.0)))
+plt.legend(legend)
+plt.setp(plt.gca().get_legend().get_texts(), fontsize='small')
+plt.title('States v Time')
 plt.xlabel('Time (ms)')
 # Show the plot
 plt.show()
