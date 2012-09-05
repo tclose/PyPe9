cad9f36ad53c54d857bff9be7485c795768c14e9
Tom Close <tclose@oist.jp>
2012-09-05 12:20:30 +0900

    Added ability to plot ionic currents to activity plot

--- src/visualize/activity_plot.py
+++ src/visualize/activity_plot.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description='A script to plot spike activity')
 parser.add_argument('filename', help='The file to plot the spikes from')
 parser.add_argument('--time_start', type=float, default=None, help='The start of the plot')
 parser.add_argument('--time_stop', type=float, default=None, help='The stop of the plot')
-parser.add_argument('--v_incr', type=float, default=0.1, help='The minimum increment required before the next step in the voltage trace is plotted')
+parser.add_argument('--incr', type=float, default=0.1, help='The minimum increment required before the next step in the voltage trace is plotted')
 parser.add_argument('--extra_label', type=str, default='', help='Additional label information')
 args = parser.parse_args()
 
@@ -82,7 +82,7 @@ if variable == 'spikes':
 elif variable == 'v':
     if not file_header.has_key('dt'):
         raise Exception("Required header field 'dt' was not found in file header.")
-    # Load voltages selectively, if the difference between previous voltage point exceeds args.v_incr
+    # Load voltages selectively, if the difference between previous voltage point exceeds args.incr
     f = open(args.filename)
     time_i = 0
     prev_ID = None
@@ -100,13 +100,13 @@ elif variable == 'v':
                     voltages[-1].append(v)
                     times[-1].append(time_i * file_header['dt'])
                 time_i = 0
-                prev_v = v - 2.0 * args.v_incr
+                prev_v = v - 2.0 * args.incr
                 prev_ID = ID
                 voltages.append([])
                 times.append([])
                 IDs.append(int(float(ID)))
-            # If the voltage change is greater than the specified v_incr add it to the vector
-            if abs(v - prev_v) > args.v_incr:
+            # If the voltage change is greater than the specified incr add it to the vector
+            if abs(v - prev_v) > args.incr:
                 voltages[-1].append(v)
                 times[-1].append(time_i * file_header['dt'])
                 prev_v = v
@@ -127,6 +127,51 @@ elif variable == 'v':
     plt.xlabel('Time (ms)')
     plt.ylabel('Soma Voltage (V)')
 else:
-    raise Exception ("Unrecognised variable '%s' (from the file extension)" % variable)
+    if not file_header.has_key('dt'):
+        raise Exception("Required header field 'dt' was not found in file header.")
+    # Load states selectistateely, if the difference betweenprevious state point exceeds args.incr
+    f = open(args.filename)
+    time_i = 0
+    prev_ID = None
+    states = []
+    times = []
+    IDs = []
+    for line in f:
+        if line[0] != '#': # Check to see if the line is a comment
+            state, ID = line.split()
+            state = float(state)
+            # If the ID signifies the start of a new cell reset the time index
+            if ID != prev_ID:
+                # If not in the initial loop, append last value/time pair to fill out the plot of theprevious ID out to the right 
+                if prev_ID != None:
+                    states[-1].append(state)
+                    times[-1].append(time_i * file_header['dt'])
+                time_i = 0
+                prev_state = state - 2.0 * args.incr
+                prev_ID = ID
+                states.append([])
+                times.append([])
+                IDs.append(int(float(ID)))
+            # If the state change is greater than the specified incr add it to the vector
+            if abs(state -prev_state) > args.incr:
+                states[-1].append(state)
+                times[-1].append(time_i * file_header['dt'])
+                prev_state = state
+            time_i += 1
+    # Append last value/time pair to fill out plot of the final ID to the right
+    states[-1].append(state)
+    times[-1].append(time_i * file_header['dt'])
+    if not states:
+        print "No trace was loaded from file"
+        sys.exit(0)
+    # Plot states sorted in order of their IDs
+    sorted_IDs = []
+    for t, state, ID in sorted(zip(times, states, IDs), key=lambda line: int(float(line[2]))):
+        plt.plot(t, state)
+        sorted_IDs.append(ID)
+    plt.legend(sorted_IDs)
+    plt.title(file_header['label'] + ' ' + args.extra_label + ' - ' + variable + ' v Time')
+    plt.xlabel('Time (ms)')
+    plt.ylabel(variable)
 # Show the plot
 plt.show()
