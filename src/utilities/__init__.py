028c553f7593e2d736b6f224517f107ea1bd2606
Tom Close <tclose@oist.jp>
2012-09-21 09:12:23 +0900

    Added convenience functions for use with iPython notebook

---
+++ src/utilities/__init__.py
@@ -0,0 +1,14 @@
+"""
+
+    This package contains general utilties methods for running and plotting scripts
+    
+    @author Tom Close
+    @date 21/9/2012
+
+"""
+
+#######################################################################################
+#
+#        Copyright 2011 Okinawa Institute of Science and Technology (OIST), Okinawa, Japan
+#
+#######################################################################################
---
+++ src/utilities/inotebook.py
@@ -0,0 +1,69 @@
+"""
+
+    This module contains methods to initiate and plot simulations for both NEURON and NEST
+
+    
+    @author Tom Close
+    @date 21/9/2012
+
+"""
+
+#######################################################################################
+#
+#        Copyright 2011 Okinawa Institute of Science and Technology (OIST), Okinawa, Japan
+#
+#######################################################################################
+
+import sys
+import os.path #@UnusedImport
+import subprocess
+from difflib import context_diff
+from visualize.activity_plot import activity_plot #@UnusedImport
+
+PROJECT_DIR = None
+
+def inotebook_init(project_dir='/home/tclose/kbrain'):
+    """
+    Adds the source directory to the system path and changes the current working directory
+    
+    @param project_dir: The project dir for the notebeook
+    """
+    sys.modules[__name__].PROJECT_DIR=project_dir
+    os.chdir(project_dir)
+    sys.path.append(os.path.join(project_dir, 'src'))
+    git_rev = subprocess.check_output('git rev-parse HEAD', shell=True)
+    print "Using git revision of k-Brain: {git_rev}".format(git_rev=git_rev)
+
+def diff(ref_file, new_file):
+    """
+    Performs a 'context diff' on the two files provided. Useful for comparing to scripts before
+    comparing their output
+    
+    @param file1: The reference file
+    @param new_file: The file to compare agains the reference
+    """
+    ref_file_contents = open(ref_file).readlines()
+    new_file_contents = open(new_file).readlines()
+    for line in context_diff(ref_file_contents, new_file_contents):
+        sys.stdout.write(line)
+        
+def run(cmd):
+    """
+    Runs a command in subprocess in a bash shell and returns the output along with the stderr even
+    if there is an exception thrown
+    
+    @param cmd: The command string to be run in bash
+    """
+    try:
+        output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+    print output
+    
+def clear_output_dir(*files):
+    for f in files:
+        try:
+            os.remove(os.path.join(PROJECT_DIR, 'output', f))
+        except OSError:
+            pass
+    print os.listdir(os.path.join(PROJECT_DIR, 'output'))
