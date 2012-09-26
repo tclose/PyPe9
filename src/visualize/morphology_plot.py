e39d86ee3559696b7f304d6506ab2de4a59c53f7
Tom Close <tclose@oist.jp>
2012-09-26 16:55:57 +0900

    Getting new neuron.ncml.Segment structure working

--- src/visualize/morphology_plot.py
+++ src/visualize/morphology_plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Contains a method for plotting cell positions loaded from BRep export files
+Contains a method for plotting morphologies of NINEML+ network
 
 @author Tom Close
 
@@ -14,23 +14,15 @@ Contains a method for plotting cell positions loaded from BRep export files
 
 import sys
 import os.path
-import numpy as np
-import matplotlib.pyplot as plt
+from neuronvisio.controls import Controls
 import argparse
-from mpl_toolkits.mplot3d import Axes3D #@UnusedImport
-from copy import copy
-
-def quit_figure(event):
-    """
-    Creates a shortcut to close the current window with the key 'q'
-    """
-    if event.key == 'q':
-        plt.close(event.canvas.figure)
 
 def main(arguments):  
     PROJECT_PATH = os.path.normpath(os.path.join(os.path.realpath(__file__), '..', '..', '..'))
-    parser = argparse.ArgumentParser(description='A script to plot morphology of neuron cells')
+    parser = argparse.ArgumentParser(description=__doc__)
     args = parser.parse_args(arguments)
+
+
 
 
 if __name__ == "__main__":
