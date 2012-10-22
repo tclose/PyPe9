eb951052698f42ebc3632fffd28d5d45aa4f49e2
Tom Close <tclose@oist.jp>
2012-10-22 15:20:59 +0900

    Added flag (and new flag functionality) to allow switching off of granule to golgi connections

--- src/simulate/fabios_network.py
+++ src/simulate/fabios_network.py
@@ -39,7 +39,8 @@ parser.add_argument('--para_unsafe', action='store_true', help='If set the netwo
 parser.add_argument('--volt_trace', metavar=('POPULATION', 'INDEX'), nargs=2, default=[], action='append', help='Save voltage traces for the given list of ("population name", "cell ID") tuples')
 parser.add_argument('--debug', action='store_true', help='Loads a stripped down version of the network for easier debugging')
 parser.add_argument('--silent_build', action='store_true', help='Suppresses all build output')
-parser.add_argument('--include_gap', action='store_true', help='Includes gap junctions into the network')
+parser.add_argument('--include_gap', action='store_true', help='Includes Golgi-to-Golgi gap junctions into the network')
+parser.add_argument('--no_granule_to_golgi', action='store_true', help='Deactivates the granule to golgi connection in the network.')
 args = parser.parse_args()
 # Set the network xml location
 if args.debug:
@@ -64,6 +65,8 @@ exec("from ninemlp.%s import *" % args.simulator)
 flags = []
 if args.include_gap:
     flags.append('includeGap')
+if args.no_granule_to_golgi:
+    flags.append(('GranuleToGolgi', False))
 # Build the network
 print "Building network"
 net = Network(network_xml_location, timestep=args.timestep, min_delay=args.min_delay, max_delay=2.0, #@UndefinedVariable
