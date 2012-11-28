0f852a99ce0b971e9b8141fa00d64b96a86e7471
Tom Close <tclose@oist.jp>
2012-11-28 10:53:09 +0900

    fixed up morphology plot

--- src/visualize/morphology_plot.py
+++ src/visualize/morphology_plot.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 """
-Contains a method for plotting morphologies of NINEML+ network
+Contains a method for plotting morphologies of NINEML+ network. The selected cell model 
+will be loaded along with NeuronVisio's controls, from which the morphology can be plotted using the
+Plot3D function (as a bonus the cell can also be run interactively using the other NeuronVisio 
+commands). 
+
+Requires NeuronVisio,to install NeuronVisio see http://mattions.github.com/neuronvisio/install.html.
 
 @author Tom Close
 
 """
-
 #######################################################################################
 #
 #    Copyright 2011 Okinawa Institute of Science and Technology (OIST), Okinawa, Japan
 #
 #######################################################################################
-
 import sys
-import os.path
 import argparse
 
-PROJECT_PATH = os.path.normpath(os.path.join(os.path.realpath(__file__), '..', '..', '..'))
-
 def main(arguments):  
+    """
+    The main function that actually runs the script
+    """
+    # Set up the argument parser
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('xml_filename', type=str, 
                         help="The name of the xml file containing the morphology to plot")
