6c0e5dcfff1d57e06af1c040251141e0799caa48
Tom Close <tclose@oist.jp>
2012-12-10 17:04:26 +0900

    Implemented ElectricalSyanpseProjection class as cleaner and more complete method of implementing gap junctions. However it is still not working with the pointers used to set the source and target vars being all seemingly pointing to the same spot (which is 0V)

--- src/test/gap_test.py
+++ src/test/gap_test.py
@@ -14,11 +14,11 @@
 import os.path
 import argparse
 import ninemlp
+from ninemlp.neuron import * #@UnusedWildImport
 PROJECT_PATH = os.path.normpath(os.path.join(ninemlp.SRC_PATH, '..'))
 def main(arguments):
     parser = argparse.ArgumentParser(description=__doc__)
     args = parser.parse_args(arguments) #@UnusedVariable
-    from ninemlp.neuron import * #@UnusedWildImport
     print "Building network"
     net = Network(os.path.join(PROJECT_PATH, 'xml', 'cerebellum', 'gap_test.xml'),
                   timestep=0.002, min_delay=0.0025, max_delay=2.0, temperature=25.0,
@@ -26,12 +26,12 @@ def main(arguments):
     test1 = net.get_population('Test1')
     test2 = net.get_population('Test2')
     current_source = StepCurrentSource({'amplitudes': [1.0],  #@UndefinedVariable
-                                        'times': [1000]}) 
+                                        'times': [100]}) 
     test2.inject(current_source)
     test1.record_v()
     test2.record_v()
     net.describe()
-    run(2000) #@UndefinedVariable
+    run(200) #@UndefinedVariable
     test1.print_v(os.path.join(PROJECT_PATH, 'output', 'Test1.v'))
     test2.print_v(os.path.join(PROJECT_PATH, 'output', 'Test2.v'))
     print "Simulated gap test network"
