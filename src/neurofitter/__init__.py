3f5e837e9b8db79ef9aee110c91f28e4bdf02fbb
Tom Close <tclose@oist.jp>
2013-01-15 17:39:49 +0900

    Added stub for neurofitter module

---
+++ src/neurofitter/__init__.py
@@ -0,0 +1,13 @@
+"""
+
+Prepares the environment and parameter scripts to NeuroFitter
+
+Author: Tom Close
+Date: 15/1/2013
+Email: tclose@oist.jp
+"""
+
+import sys
+import os
+import argparse
+import numpy as np
