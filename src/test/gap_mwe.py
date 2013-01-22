9e27866ffab2d0dfe81202ea323172227bd697ae
Tom Close <tclose@oist.jp>
2013-01-22 12:31:01 +0900

    Fixed up gap mod file so that it now works with MPI initialised==true

--- src/test/gap_mwe.py
+++ src/test/gap_mwe.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 
 A minimum working example of NEURON gap junctions over MPI
@@ -17,39 +18,33 @@ import numpy as np
 try:
     from mpi4py import MPI #@UnresolvedImport @UnusedImport
 except:
-    print "mpi4py was not found, MPI will remain disabled if MPI initialized==False on startup"
+    print "mpi4py was not found, MPI will remain disabled if MPI initialized==false on startup"
 from neuron import h, load_mechanisms
 h.load_file('stdrun.hoc')
 from ninemlp.neuron.build import compile_nmodl
-
 # The GID used for the gap junction connection. NB: this number is completely independent from the 
 # GID's used for NEURON sections.
 GID_FOR_VARS = 0
-
 # Arguments to the script
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--plot', action='store_true', help="Plot the data instead of saving it")
-parser.add_argument('--output_dir', type=str, default=os.getcwd(), 
+parser.add_argument('--output_dir', type=str, default=os.getcwd(),
                     help="The directory to save the output files into")
 parser.add_argument('--gap_mechanism_dir', type=str, default=os.getcwd(),
                     help="The directory to load the gap mechanism from")
-parser.add_argument('--build', type=str, default='lazy', 
+parser.add_argument('--build', type=str, default='lazy',
                     help="Can be either 'lazy', 'build_only', or 'require' (used for separating "
-                         "compilation and running)") 
+                         "compilation and running)")
 args = parser.parse_args()
-
+# Compile the gap mechanisms
 compile_nmodl(args.gap_mechanism_dir, build_mode=args.build)
 if args.build == 'build_only':
     sys.exit(0)
-# Load gap mechanism from another directory if required    
-#if args.gap_mechanism_dir is not os.getcwd():
-#    load_mechanisms(args.gap_mechanism_dir)
 # Get the parallel context and related parameters
 pc = h.ParallelContext()
 num_processes = int(pc.nhost())
 mpi_rank = int(pc.id())
-print "On process {} of {}".format(mpi_rank+1, num_processes)
-
+print "On process {} of {}".format(mpi_rank + 1, num_processes)
 print "Creating test network..."
 # The pre-synaptic cell is created on the first node and the post-synaptic cell on the last node 
 # (NB: which will obviously be the same if there is only one node)
@@ -58,8 +53,9 @@ if mpi_rank == 0:
     # Create the pre-synaptic cell
     pre_cell = h.Section()
     pre_cell.insert('pas')
+#    nc1 = h.NetCon(pre_cell(0.5)._ref_v, None, sec=pre_cell)
     # Connect the voltage of the pre-synaptic cell to the gap junction on the post-synaptic cell
-    pc.source_var(pre_cell(0.5)._ref_v, GID_FOR_VARS)    
+    pc.source_var(pre_cell(0.5)._ref_v, GID_FOR_VARS)
     # Stimulate the first cell to make it obvious whether gap junction is working
     stim = h.IClamp(pre_cell(0.5))
     stim.delay = 50
@@ -72,7 +68,8 @@ if mpi_rank == (num_processes - 1):
     print "Creating post cell on process {}".format(mpi_rank)
     # Create the post-synaptic cell
     post_cell = h.Section()
-    post_cell.insert('pas')    
+    post_cell.insert('pas')
+#    nc = h.NetCon(post_cell(0.5)._ref_v, None, sec=post_cell)
     # Insert gap junction
     gap_junction = h.gap(0.5, sec=post_cell)
     gap_junction.g = 1.0
@@ -82,18 +79,18 @@ if mpi_rank == (num_processes - 1):
     post_v = h.Vector()
     post_v.record(post_cell(0.5)._ref_v)
 # Finalise construction of parallel context
-pc.setup_transfer()    
+pc.setup_transfer()
 # Record time
 rec_t = h.Vector()
-rec_t.record(h._ref_t)    
+rec_t.record(h._ref_t)
 print "Finished network construction on process {}".format(mpi_rank)
-    
+
 # Run simulation    
 print "Setting maxstep on process {}".format(mpi_rank)
 pc.set_maxstep(10)
 print "Finitialise on process {}".format(mpi_rank)
-#h.finitialize(-60)
-h.stdinit()
+h.finitialize(-60)
+#h.stdinit()
 print "Solving on process {}".format(mpi_rank)
 pc.psolve(100)
 print "Running worker on process {}".format(mpi_rank)
@@ -130,9 +127,9 @@ else:
     # Save data
     print "Saving data..."
     if mpi_rank == 0:
-        np.savetxt(os.path.join(args.output_dir, "pre_v.dat"), 
-                   np.transpose(np.vstack((t_array, pre_v_array)))) 
+        np.savetxt(os.path.join(args.output_dir, "pre_v.dat"),
+                   np.transpose(np.vstack((t_array, pre_v_array))))
     if mpi_rank == (num_processes - 1):
-        np.savetxt(os.path.join(args.output_dir, "post_v.dat"), 
+        np.savetxt(os.path.join(args.output_dir, "post_v.dat"),
                    np.transpose(np.vstack((t_array, post_v_array))))
 print "Done."
