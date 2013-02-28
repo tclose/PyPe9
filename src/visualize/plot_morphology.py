021ed33c8cd486ba334346b2ee9c89274554bbd9
Tom Close <tclose@oist.jp>
2013-02-28 14:50:50 +0900

    cleaned up plot_connectivity and plot_morphology so that they can be called as a python function as well as a stand alone script

--- src/visualize/plot_morphology.py
+++ src/visualize/plot_morphology.py
@@ -15,13 +15,26 @@ Requires NeuronVisio,to install NeuronVisio see http://mattions.github.com/neuro
 #    Copyright 2011 Okinawa Institute of Science and Technology (OIST), Okinawa, Japan
 #
 #######################################################################################
-import sys
-import argparse
 
-def main(arguments):  
+from ninemlp.neuron.ncml import load_cell_type
+
+def plot_morphology(xml_filename, cell_id, morph_id=None):  
     """
     The main function that actually runs the script
     """
+    # Import NeuroVisio gui
+    from neuronvisio.controls import Controls
+    # Start the GUI
+    controls = Controls() #@UnusedVariable: Just needs to be created for the GUI to be initialised
+    # Load the cell
+    Cell = load_cell_type(cell_id, xml_filename, morph_id)
+    cell = Cell() #@UnusedVariable: Just needs to be created for the morphology to show up
+    # Pause the script to allow inspection of the morphology
+    raw_input("<press the 'Plot3D' button on the control panel to plot morphology, then press " \
+              " return to continue>")
+
+if __name__ == "__main__":
+    import argparse
     # Set up the argument parser
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('xml_filename', type=str, 
@@ -29,18 +42,5 @@ def main(arguments):
     parser.add_argument('cell_id', type=str, help="The ID of the cell to extract from the xml file")
     parser.add_argument('--morph_id', type=str, default=None, 
                         help='The morphology ID to load from the xml file')
-    args = parser.parse_args(arguments)
-    # Import NeuroVisio gui
-    from neuronvisio.controls import Controls
-    from ninemlp.neuron.ncml import load_cell_type
-    # Start the GUI
-    controls = Controls() #@UnusedVariable: Just needs to be created for the GUI to be initialised
-    # Load the cell
-    Cell = load_cell_type(args.cell_id, args.xml_filename, args.morph_id)
-    cell = Cell() #@UnusedVariable: Just needs to be created for the morphology to show up
-    # Pause the script to allow inspection of the morphology
-    raw_input("<press the 'Plot3D' button on the control panel to plot morphology, then press " \
-              " return to continue>")
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+    args = parser.parse_args()
+    plot_morphology(args.xml_filename, args.cell_id, args.morph_id)
