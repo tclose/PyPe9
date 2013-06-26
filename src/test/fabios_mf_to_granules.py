16a5fae3017d7c6c1aa7da5d7120f8fc1387c9b2
Tom Close <tclose@oist.jp>
2013-06-26 22:47:13 +0900

    trying to sort out local projection for mf_to_granules

--- src/test/fabios_mf_to_granules.py
+++ src/test/fabios_mf_to_granules.py
@@ -103,9 +103,9 @@ for times in args.spike_times:
     spike_times[ID] = Sequence(times[1:])
     conn_list = np.array(proj.get([], 'list'), dtype=int)
     post_proj = conn_list[np.where(conn_list[:,0] == ID), 1]
-    view = granules[post_proj]
-    if np.count_nonzero(view._mask_local):
-        view.record_v()
+    local_proj = post_proj[granules._mask_local[post_proj]]
+    if len(local_proj):
+        granules[local_proj].record_v()
 mossy_fibers.set(spike_times=spike_times)    
 print "Network description"
 net.describe()
