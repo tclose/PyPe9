1505c237a90f7b8f7450afafed876b72d44e574e
Tom Close <tclose@oist.jp>
2013-07-03 12:27:50 +0900

    changed number of cells to 8100

--- src/test/dummy.py
+++ src/test/dummy.py
@@ -2,7 +2,7 @@
 from pyNN.neuron import Population, setup, run, h
 from pyNN.parameters import Sequence
 from pyNN.neuron.standardmodels.cells import SpikeSourceArray
-NUM_CELLS=100
+NUM_CELLS=8100
 setup(1, 1.5, 100)
 spike_times = []
 for i in xrange(NUM_CELLS):
