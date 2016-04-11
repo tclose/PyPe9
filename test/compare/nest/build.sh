b53b8a3a19929eec5dc4d997a75d104962386a5b
Tom Close <tclose@oist.jp>
2016-04-11 17:10:45 +1000

    fixed bug in build.sh and added xcode project to gitignore

--- test/compare/nest/build.sh
+++ test/compare/nest/build.sh
@@ -3,6 +3,7 @@ aclocal \
 && automake --add-missing \
 && autoconf
 rm -r build/*
+mkdir build
 cd build
 CPPFLAGS=-DDEBUG CFLAGS='-g3 -O0' CXXFLAGS='-g3 -O0' ../configure --prefix=`pwd`/..
 make install
