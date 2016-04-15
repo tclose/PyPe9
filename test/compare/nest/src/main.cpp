9e1580256d79d711af96b6adcd43d34f0c1c2c9e
Tom Close <tclose@oist.jp>
2016-04-15 14:30:06 +1000

    added macros for fast switching between master and branch

--- test/compare/nest/src/main.cpp
+++ test/compare/nest/src/main.cpp
@@ -15,14 +15,21 @@
 #include "mock_nest.h"
 #include "conditions.h"
 
+//#define MASTER
+#define BRANCH
+
 
 int main(void) {
 
     std::cout << "Create model objects" << std::endl;
     
     // Initialise models
+#ifdef MASTER
     nineml::Master master;
-    //nineml::Branch branch;
+#endif
+#ifdef BRANCH
+    nineml::Branch branch;
+#endif
     
     std::cout << "Set Status" << std::endl;
     
@@ -30,26 +37,40 @@ int main(void) {
     set_status(status);  // From custom "status.h"
     DictionaryDatum status_datum(&status);
 
+#ifdef MASTER
     master.set_status(status_datum);
-    //branch.set_status(status_datum);
+#endif
+#ifdef BRANCH
+    branch.set_status(status_datum);
+#endif
 
     std::cout << "Initialise buffers" << std::endl;
     
     // Init Buffers of models
+#ifdef MASTER
     master.init_buffers_();
-    //branch.init_buffers_();
+#endif
+#ifdef BRANCH
+    branch.init_buffers_();
+#endif
 
     std::cout << "Calibrate" << std::endl;
     
+#ifdef MASTER
     master.calibrate();
-    //branch.calibrate();
+#endif
+#ifdef BRANCH
+    branch.calibrate();
+#endif
 
-    double dt = 0.025;
-    
     std::cout << "Set current and event buffers" << std::endl;
     
+#ifdef MASTER
     set_ring_buffers<nineml::Master>(master);
-    //set_ring_buffers<nineml::Branch>(branch);
+#endif
+#ifdef BRANCH
+    set_ring_buffers<nineml::Branch>(branch);
+#endif
     
     std::cout << "Run update steps" << std::endl;
 
@@ -57,14 +78,23 @@ int main(void) {
     
     for (int i = 0; i < NUM_SLICES; ++i) {
 
+#ifdef MASTER
         master.update(origin, nest::Scheduler::min_delay * i, nest::Scheduler::min_delay * (i + 1));
-        //branch.update(origin, from, to);
+#endif
+#ifdef BRANCH
+        branch.update(origin, nest::Scheduler::min_delay * i, nest::Scheduler::min_delay * (i + 1));
+#endif
 
     }
 
     std::cout << "To plot comparison:" << std::endl;
-    std::cout << "plot_comparison.py " << get_data_path<nineml::Master>();
-    //std::cout << " " << get_data_path<nineml::Branch>();
+    std::cout << "plot_comparison.py";
+#ifdef MASTER
+    std::cout << " " << get_data_path<nineml::Master>();
+#endif
+#ifdef BRANCH
+    std::cout << " " << get_data_path<nineml::Branch>();
+#endif
     std::cout << std::endl;
     
 	return 0;
