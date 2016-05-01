94840259316d354b378b77a99ebd612c0a4ee8ad
Tom Close <tclose@oist.jp>
2016-05-01 14:26:16 +1000

    spitting mock_nest and mock_sli into separate headers

--- test/compare/nest/src/universal_data_logger.h
+++ test/compare/nest/src/universal_data_logger.h
@@ -26,29 +26,6 @@ namespace nest {
         NodeType* node_;
     };
 
-    template <class NodeType> UniversalDataLogger<NodeType>::UniversalDataLogger(NodeType& node)
-      : node_(&node) {
-        std::string path = ::get_data_path<NodeType>();
-        std::cout << "Writing output to " << path << std::endl;
-        output_file = new std::ofstream(path);
-    }
-
-    template <class NodeType> UniversalDataLogger<NodeType>::~UniversalDataLogger() {
-        delete output_file;
-    }
-
-    template <class NodeType> void UniversalDataLogger<NodeType>::record_data(long_t step) {
-        if (!step) {
-            (*output_file) << "# ";
-            for (typename RecordablesMap<NodeType>::iterator it = node_->recordablesMap_.begin(); it != node_->recordablesMap_.end(); ++it)
-                (*output_file) << it->first << " ";
-            (*output_file) << std::endl;
-        }
-        for (typename RecordablesMap<NodeType>::iterator it = node_->recordablesMap_.begin(); it != node_->recordablesMap_.end(); ++it)
-            (*output_file) << ((*node_).*(it->second))()  << " ";
-        (*output_file) << std::endl;
-    }
-
 }
 
 #endif
